e0919f275103940c763994517fcb37b5583238c2
CarolVasquezB <karito_v19@hotmail.com>
2017-05-13 12:37:02 -0500

    Update InventarioBD_Tirsa.sql

--- InventarioBD_Tirsa.sql
+++ InventarioBD_Tirsa.sql
@@ -6,6 +6,7 @@ USE `inventario`;
 -- ------------------------------------------------------
 -- Server version	5.6.24-log
 --modificado el 13/05/2017
+--prueba sincronizacion
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
